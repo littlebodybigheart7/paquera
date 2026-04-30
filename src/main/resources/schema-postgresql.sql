f72b13cbd25931139b669a90009feed972f46dcc
Bruna <142622487+littlebodybigheart7@users.noreply.github.com>
2026-04-30 18:13:35 -0300

    Update schema-postgresql.sql

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -1,6 +1,7 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 CREATE TABLE IF NOT EXISTS paquera(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     nome VARCHAR(100) NOT NULL,
-    idade  VARCHAR(11)  NOT NULL 
+    idade VARCHAR(11) NOT NULL
 );
-
