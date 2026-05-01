d65c0359e22299c094aa939e06919195ba738744
Bruna <142622487+littlebodybigheart7@users.noreply.github.com>
2026-05-01 17:02:38 -0300

    Update Paquera.java

--- src/main/java/com/example/aula1/model/Paquera.java
+++ src/main/java/com/example/aula1/model/Paquera.java
@@ -7,17 +7,18 @@ import java.util.UUID;
 
 public class Paquera {
 
-    private String id, nome, idade, signo, gostaDe, naoGostaDe;
+    private String id, nome, idade, signo, gostaDe, naoGostaDe, foto;
 
     public Paquera() {}
 
-    public Paquera(String id, String nome, String idade, String signo, String gostaDe, String naoGostaDe) {
+    public Paquera(String id, String nome, String idade, String signo, String gostaDe, String naoGostaDe, String foto) {
         this.id = id;
         this.nome = nome;
         this.idade = idade;
         this.signo = signo;
         this.gostaDe = gostaDe;
         this.naoGostaDe = naoGostaDe;
+        this.foto = foto;
     }
 
     public String getId() { return id; }
@@ -26,6 +27,7 @@ public class Paquera {
     public String getSigno() { return signo; }
     public String getGostaDe() { return gostaDe; }
     public String getNaoGostaDe() { return naoGostaDe; }
+    public String getFoto() { return foto; }
 
     public void setId(String id) { this.id = id; }
     public void setNome(String nome) { this.nome = nome; }
@@ -33,6 +35,7 @@ public class Paquera {
     public void setSigno(String signo) { this.signo = signo; }
     public void setGostaDe(String gostaDe) { this.gostaDe = gostaDe; }
     public void setNaoGostaDe(String naoGostaDe) { this.naoGostaDe = naoGostaDe; }
+    public void setFoto(String foto) { this.foto = foto; }
 
     public static Paquera converter(Map<String, Object> registro) {
         UUID id = (UUID) registro.get("id");
@@ -41,7 +44,8 @@ public class Paquera {
         String signo = (String) registro.get("signo");
         String gostaDe = (String) registro.get("gostade");
         String naoGostaDe = (String) registro.get("naogostade");
-        return new Paquera(id.toString(), nome, idade, signo, gostaDe, naoGostaDe);
+        String foto = (String) registro.get("foto");
+        return new Paquera(id.toString(), nome, idade, signo, gostaDe, naoGostaDe, foto);
     }
 
     public static ArrayList<Paquera> converterTodos(List<Map<String, Object>> registros) {
