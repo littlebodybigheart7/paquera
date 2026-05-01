1e5a293da3ccc4fe5718324c4954aeeb9489e0ca
Bruna <142622487+littlebodybigheart7@users.noreply.github.com>
2026-05-01 18:57:55 -0300

    Update PaqueraDAO.java

--- src/main/java/com/example/aula1/model/PaqueraDAO.java
+++ src/main/java/com/example/aula1/model/PaqueraDAO.java
@@ -12,8 +12,10 @@ public class PaqueraDAO {
     @Autowired
     private JdbcTemplate jdbc;
 
+    // INSERT
     public void inserirPaquera(Paquera paquera) {
-        String sql = "INSERT INTO paquera(nome, idade, signo, gostade, naogostade, foto) VALUES (?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO paquera(id, nome, idade, signo, gostade, naogostade, foto) VALUES (gen_random_uuid(), ?, ?, ?, ?, ?, ?)";
+        
         Object[] obj = new Object[6];
         obj[0] = paquera.getNome();
         obj[1] = paquera.getIdade();
@@ -21,6 +23,23 @@ public class PaqueraDAO {
         obj[3] = paquera.getGostaDe();
         obj[4] = paquera.getNaoGostaDe();
         obj[5] = paquera.getFoto();
+
+        jdbc.update(sql, obj);
+    }
+
+    // UPDATE
+    public void atualizarPaquera(Paquera paquera) {
+        String sql = "UPDATE paquera SET nome=?, idade=?, signo=?, gostade=?, naogostade=?, foto=? WHERE id = ?::uuid";
+        
+        Object[] obj = new Object[7];
+        obj[0] = paquera.getNome();
+        obj[1] = paquera.getIdade();
+        obj[2] = paquera.getSigno();
+        obj[3] = paquera.getGostaDe();
+        obj[4] = paquera.getNaoGostaDe();
+        obj[5] = paquera.getFoto();
+        obj[6] = paquera.getId();
+
         jdbc.update(sql, obj);
     }
 
