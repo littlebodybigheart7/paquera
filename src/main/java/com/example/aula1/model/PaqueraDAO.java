ae13bdf0425904799127d552abb345dd5d99182d
Bruna <142622487+littlebodybigheart7@users.noreply.github.com>
2026-04-30 22:21:04 -0300

    Update inserirPaquera method to include all fields

--- src/main/java/com/example/aula1/model/PaqueraDAO.java
+++ src/main/java/com/example/aula1/model/PaqueraDAO.java
@@ -12,17 +12,16 @@ public class PaqueraDAO {
     @Autowired
     private JdbcTemplate jdbc;
 
-    public void inserirPaquera(Paquera paquera) {
-        String sql = "INSERT INTO paquera(nome, idade) VALUES (?, ?)";
-        Object[] obj = new Object[2];
-        obj[0] = paquera.getNome();
-        obj[1] = paquera.getIdade();
-        obj[2] = paquera.getSigno();
-        obj[3] = paquera.getGostaDe();
-        obj[4] = paquera.getNaoGostaDe();
-        jdbc.update(sql, obj);
-    }
-
+public void inserirPaquera(Paquera paquera) {
+    String sql = "INSERT INTO paquera(nome, idade, signo, gostade, naogostade) VALUES (?, ?, ?, ?, ?)";
+    Object[] obj = new Object[5];
+    obj[0] = paquera.getNome();
+    obj[1] = paquera.getIdade();
+    obj[2] = paquera.getSigno();
+    obj[3] = paquera.getGostaDe();
+    obj[4] = paquera.getNaoGostaDe();
+    jdbc.update(sql, obj);
+}
     public Paquera mostrarPaquera(String uuid) {
         String sql = "SELECT * FROM paquera WHERE id = ?::uuid";
         return Paquera.converter(jdbc.queryForMap(sql, uuid));
