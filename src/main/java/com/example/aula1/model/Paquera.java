f2d29754ce74c7810cb6af46a3cfb138ada38343
Bruna <142622487+littlebodybigheart7@users.noreply.github.com>
2026-04-30 22:04:32 -0300

    Update Paquera.java

--- src/main/java/com/example/aula1/model/Paquera.java
+++ src/main/java/com/example/aula1/model/Paquera.java
@@ -7,38 +7,41 @@ import java.util.UUID;
 
 public class Paquera {
 
-    private String id, nome, idade;
+    private String id, nome, idade, signo, gostaDe, naoGostaDe;
 
-    // Construtor para o formulário (Spring MVC @ModelAttribute)
-    public Paquera() {
-    }
+    public Paquera() {}
 
-    // Construtor para SELECT (com id)
-    public Paquera(String id, String nome, String idade) {
+    public Paquera(String id, String nome, String idade, String signo, String gostaDe, String naoGostaDe) {
         this.id = id;
         this.nome = nome;
         this.idade = idade;
-    }
-
-    // Construtor para INSERT (sem id)
-    public Paquera(String nome, String idade) {
-        this.nome = nome;
-        this.idade = idade;
+        this.signo = signo;
+        this.gostaDe = gostaDe;
+        this.naoGostaDe = naoGostaDe;
     }
 
     public String getId() { return id; }
     public String getNome() { return nome; }
     public String getIdade() { return idade; }
+    public String getSigno() { return signo; }
+    public String getGostaDe() { return gostaDe; }
+    public String getNaoGostaDe() { return naoGostaDe; }
 
     public void setId(String id) { this.id = id; }
     public void setNome(String nome) { this.nome = nome; }
     public void setIdade(String idade) { this.idade = idade; }
+    public void setSigno(String signo) { this.signo = signo; }
+    public void setGostaDe(String gostaDe) { this.gostaDe = gostaDe; }
+    public void setNaoGostaDe(String naoGostaDe) { this.naoGostaDe = naoGostaDe; }
 
     public static Paquera converter(Map<String, Object> registro) {
         UUID id = (UUID) registro.get("id");
         String nome = (String) registro.get("nome");
         String idade = (String) registro.get("idade");
-        return new Paquera(id.toString(), nome, idade);
+        String signo = (String) registro.get("signo");
+        String gostaDe = (String) registro.get("gostade");
+        String naoGostaDe = (String) registro.get("naogostade");
+        return new Paquera(id.toString(), nome, idade, signo, gostaDe, naoGostaDe);
     }
 
     public static ArrayList<Paquera> converterTodos(List<Map<String, Object>> registros) {
