a85c44f6d3e09ab68065c763f636d991020ad4f5
Bruna <142622487+littlebodybigheart7@users.noreply.github.com>
2026-06-17 17:55:01 -0300

    usuario service java

---
+++ src/main/java/com/example/aula1/model/UsuarioService.java
@@ -0,0 +1,19 @@
+package com.example.aula1.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    UsuarioDAO usuarioDAO;
+
+    public void cadastrar(Usuario usuario) {
+        usuarioDAO.inserirUsuario(usuario);
+    }
+
+    public boolean usuarioJaExiste(String nome) {
+        return usuarioDAO.existeUsuario(nome);
+    }
+}
